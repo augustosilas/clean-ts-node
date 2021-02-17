ff9003ae0a459790ac6cbf492cb4d3b8c09da0b9
Silas Augusto <augusto.fernandes775@gmail.com>
2021-02-16 21:57:26 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -4,6 +4,6 @@ export class LogControllerDecorator implements Controller {
   constructor (private readonly controller: Controller) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     await this.controller.handle(httpRequest)
-    return null
+    return {} as HttpResponse
   }
 }
