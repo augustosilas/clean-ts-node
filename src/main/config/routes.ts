6a91615f44ee6a481109a52ac9a44c9f71d06a83
Silas Augusto <augusto.fernandes775@gmail.com>
2021-05-19 21:42:15 -0300

    fix: change conditions routes

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -6,7 +6,7 @@ export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   readdirSync(path.resolve(__dirname, '..', 'routes')).map(async file => {
-    if (!file.includes('.test.') && !file.endsWith('.map')) {
+    if (!file.includes('.test.')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
